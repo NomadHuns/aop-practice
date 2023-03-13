f1c5e99bb475239f5caf80f1edfad796cfca25ed
kimalss22 <kimalss22@gmail.com>
2023-03-13 14:45:16 +0900

    프로젝트 생성

---
+++ src/main/java/shop/mtcoding/aopexam/AopexamApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.aopexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopexamApplication.class, args);
+	}
+
+}
